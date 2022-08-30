b469fd2060b8c453a26607bebedbcbfda85045f8
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-08-30 11:24:20 +0530

    example of changing state using Object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ArrayExample from "./tutorial/1-useState/ArrayExample";
 import ErrorExample from "./tutorial/1-useState/ErrorExample";
+import Object from "./tutorial/1-useState/Object";
 
 function App() {
   return (
     <div className="container">
       <ErrorExample />
       <ArrayExample />
+      <Object />
     </div>
   );
 }
---
+++ src/tutorial/1-useState/Object.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Object = () => {
+  const [person, setPerson] = useState({
+    name: "Ravi",
+    age: 31,
+    message: "Random Message",
+  });
+  console.log("person Object", person);
+  const handleChangeBtn = () => {
+    // here we used spread operation because we want to change only message, not name and age. so spread operator will keep other data as it is and only changes new changed data it received.
+
+    setPerson({ ...person, message: "Special Message" });
+  };
+  return (
+    <div>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4>
+      <button className="btn" onClick={handleChangeBtn}>
+        Change Message
+      </button>
+    </div>
+  );
+};
+
+export default Object;
