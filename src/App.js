ea461d9e2777dbb2be5b7bcba126f4bd8ee69602
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-08-30 15:19:58 +0530

    basic of useEffect and with changing document title on each rendering

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ArrayExample from "./tutorial/1-useState/ArrayExample";
 import ErrorExample from "./tutorial/1-useState/ErrorExample";
 import Object from "./tutorial/1-useState/Object";
 import SimpleCounter from "./tutorial/1-useState/SimpleCounter";
+import UseEffectBasic from "./tutorial/2-useEffect/UseEffectBasic";
 
 function App() {
   return (
     <div className="container">
-      <ErrorExample />
+      {/* <ErrorExample />
       <ArrayExample />
       <Object />
-      <SimpleCounter />
+      <SimpleCounter /> */}
+      <UseEffectBasic />
     </div>
   );
 }
