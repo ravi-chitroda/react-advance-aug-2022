16c0527fe2f9dc872ddb9ccc4d8c1fdbc1716cc6
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-01 16:20:30 +0530

    mistakes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import UseEffectBasic from "./tutorial/2-useEffect/UseEffectBasic";
 import UseEffectCleanUp from "./tutorial/2-useEffect/UseEffectCleanUp";
 import UseEffectFetch from "./tutorial/2-useEffect/UseEffectFetch";
 import UseRef from "./tutorial/4-useRef/UseRef";
-import MultipleReturn from "./tutorial/3-Conditional Rendering/MultipleReturn";
-import ShortCircuit from "./tutorial/3-Conditional Rendering/ShortCircuit";
-import ShowHide from "./tutorial/3-Conditional Rendering/ShowHide";
-import Setup from "./tutorial/5-useReducer/setup";
+import MultipleReturn from "./tutorial/Conditional Rendering/MultipleReturn";
+import ShortCircuit from "./tutorial/Conditional Rendering/ShortCircuit";
+import ShowHide from "./tutorial/Conditional Rendering/ShowHide";
 
 function App() {
   return (
@@ -19,14 +18,15 @@ function App() {
       <ArrayExample />
       <Object />
       <SimpleCounter /> */}
+
       {/* <UseEffectBasic />
       <UseEffectCleanUp />
       <UseEffectFetch /> */}
+
       {/* <MultipleReturn />
       <ShortCircuit />
       <ShowHide /> */}
-      {/* <UseRef /> */}
-      <Setup />
+      <UseRef />
     </div>
   );
 }
--- src/tutorial/5-useReducer/setup/Modal.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Modal = ({ modalContent }) => {
-  return <div className="modal">{modalContent}</div>;
-};
-
-export default Modal;
