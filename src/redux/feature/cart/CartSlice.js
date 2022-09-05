272c2fb13a4d0b3872edca073fbe40fed1f93501
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-05 22:08:07 +0530

    add Cart Items from local data

--- src/redux/feature/cart/CartSlice.js
+++ src/redux/feature/cart/CartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   total: 0,
   amount: 4,
   isLoading: true,
